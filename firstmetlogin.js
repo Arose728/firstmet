172a0bca8d37495ac53f7a55ff4df4f623257b09
Arose728 <arose728@gmail.com>
2017-01-31 10:26:38 -0500

    firstmet loging
    login demo

---
+++ firstmetlogin.js
@@ -0,0 +1,31 @@
+/*
+Firstmet interview
+*/
+
+module.exports = {
+  'open Firstmet.com' : function (client) {
+    
+    client
+      .url('https://firstmet.com/')
+      //.maximizeWindow()
+      .url(function(result){
+        console.log(result);
+      })
+      .waitForElementPresent('body', 2000)
+      .saveScreenshot('./screenshots/Firstmet1.png')
+      .useXpath()
+      .click('//*[@id="aui"]/div[4]/header/div[2]/div/a')
+      .useCss()
+      .saveScreenshot('./screenshots/firstmet2.png')
+      .setValue('input[name=email]', ['arose728@gmail.com'])
+      .setValue('input[name=password]', ['password'])
+      //.saveScreenshot('./screenshots/firstmet' n++ + '.png')
+      /*.click('button[value=submit]', function(result) {
+        this.assert.strictEqual(result.status, 0);
+      })
+      */
+      .useXpath()
+      .click('//*[@id="aui"]/div[4]/header/div[2]/div/form/fieldset[2]/input[4]')
+    }
+
+};
